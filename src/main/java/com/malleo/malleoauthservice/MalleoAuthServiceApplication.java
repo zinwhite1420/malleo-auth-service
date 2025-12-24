eb39d5b5d72c6f8aae4f1d6dbcea699804ae53ba
zinwhite1420 <zinwhite1420@gmail.com>
2025-12-24 13:43:28 +0900

    Initial commit

---
+++ src/main/java/com/malleo/malleoauthservice/MalleoAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.malleo.malleoauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalleoAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalleoAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malleo/malleoauthservice/MalleoAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malleo.malleoauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalleoAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
